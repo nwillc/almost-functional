45bb5e348971900a731e73e09a2cbbd898122fbb
Nicholas Christopher <nchristopher@amplify.com>
2014-05-06 14:10:12 -0400

    Added example code

---
+++ example/JarInfo/src/main/java/jarinfo/JarInfo.java
@@ -0,0 +1,57 @@
+package jarinfo;
+
+import almost.functional.*;
+import almost.functional.utils.SupplierIterable;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import static almost.functional.utils.ArrayIterable.newIterable;
+import static almost.functional.utils.Iterables.*;
+
+public class JarInfo {
+    public static void main(String[] args) throws Exception {
+        final JarFile jarFile = new JarFile(get(newIterable(args),0).orElseSupplier(new Supplier<String>() {
+            @Override
+            public String get() {
+                throw new IllegalArgumentException("Requires a jar file as first argument");
+            }
+        }));
+
+        final String searchName = get(newIterable(args), 1).orElse("META-INF/MANIFEST.MF");
+
+        final JarEntry searchResult = find(iterable(jarFile.entries()), new Predicate<JarEntry>() {
+            @Override
+            public boolean test(JarEntry jarEntry) {
+                return jarEntry.getName().equals(searchName);
+            }
+        }).orElseSupplier(new Supplier<JarEntry>() {
+            @Override
+            public JarEntry get() {
+                throw new IllegalArgumentException("Manifest " + searchName + " not found!");
+            }
+        });
+
+        try (InputStream inputStream = jarFile.getInputStream(searchResult);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+           forEach(new SupplierIterable<>(new Supplier<Optional<String>>() {
+                @Override
+                public Optional<String> get() {
+                    try {
+                        return Optional.ofNullable(reader.readLine());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }), new Consumer<String>() {
+               @Override
+               public void accept(String s) {
+                   System.out.println(s);
+               }
+           });
+        }
+    }
+}
