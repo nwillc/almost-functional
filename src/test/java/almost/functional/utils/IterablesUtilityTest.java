c6c45d6044d589958b7bcb41e0f092f28fdcbc23
N Christopher <nwillc@gmail.com>
2015-06-01 17:25:40 -0400

    More contract tests

---
+++ src/test/java/almost/functional/utils/IterablesUtilityTest.java
@@ -0,0 +1,10 @@
+package almost.functional.utils;
+
+import com.github.nwillc.contracts.UtilityClassContract;
+
+public class IterablesUtilityTest extends UtilityClassContract {
+    @Override
+    public Class<?> getClassToTest() {
+        return Iterables.class;
+    }
+}
---
+++ src/test/java/almost/functional/utils/IterationsUtilityTest.java
@@ -0,0 +1,10 @@
+package almost.functional.utils;
+
+import com.github.nwillc.contracts.UtilityClassContract;
+
+public class IterationsUtilityTest extends UtilityClassContract {
+    @Override
+    public Class<?> getClassToTest() {
+        return Iterators.class;
+    }
+}
