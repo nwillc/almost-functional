32ce01453a9c49d575e2fec363d79aa02cdefff9
Nicholas Christopher <nwillc@gmail.com>
2016-03-08 10:34:49 -0500

    Making preconditions use predicates

--- src/main/java/almost/functional/utils/Preconditions.java
+++ src/main/java/almost/functional/utils/Preconditions.java
@@ -17,58 +17,98 @@
 
 package almost.functional.utils;
 
+import almost.functional.Predicate;
+
+import java.lang.reflect.Constructor;
+
 /**
  * Utility operations designed to be used as precondition tests.
  */
 public final class Preconditions {
 
-	private Preconditions() {
-	}
+    private Preconditions() {
+    }
 
-	/**
-	 * Check that a reference is not null, throwing a IllegalArgumentException if it is.
-	 *
-	 * @param reference    The reference to check
-	 * @param errorMessage the message for the exception if the reference is null
-	 * @param <T>          the type of the reference
-	 * @throws java.lang.IllegalArgumentException if the reference is null
-	 */
-	public static <T> T checkNotNull(final T reference, final String errorMessage) {
-		if (reference == null) {
-			throw new IllegalArgumentException(errorMessage);
-		}
-		return reference;
-	}
+    /**
+     * Test a Predicate against a reference value, and if it fails throw a runtime exception with a message.
+     * @param reference  the value to test
+     * @param predicate  the predicate test to invoke
+     * @param ex  The RuntimeException to use
+     * @param msg The message to add to the exception
+     * @param <T> The type of the reference value
+     * @return The reference value
+     * @throws RuntimeException  An instance of ex if the predicate fails
+     */
+    public static <T> T precondition(T reference, Predicate<T> predicate, Class<? extends RuntimeException> ex, String msg)
+            throws RuntimeException {
+        if (predicate.test(reference)) {
+            return reference;
+        }
 
-	/**
-	 * Check that a String is not empty after removing whitespace.
-	 *
-	 * @param reference    the string
-	 * @param errorMessage the message for the exception
-	 * @throws java.lang.NullPointerException     if the String is null
-	 * @throws java.lang.IllegalArgumentException if the string is non-null but zero length
-	 */
-	public static String checkNonEmptyString(final String reference, final String errorMessage) {
-		for (Character c : checkNotNull(reference, errorMessage).toCharArray()) {
-			if (!Character.isWhitespace(c)) {
-				return reference;
-			}
-		}
-		throw new IllegalArgumentException(errorMessage);
-	}
+        final Constructor<? extends RuntimeException> constructor;
+        try {
+            constructor = ex.getConstructor(String.class);
+            throw constructor.newInstance(msg);
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(msg);
+        }
+    }
 
-	/**
-	 * Check that one class is assignable to another.
-	 *
-	 * @param fromValue the class assigning from
-	 * @param toValue   the class assigning to
-	 * @param message   the message used in the exception if thrown
-	 * @throws ClassCastException if the assignment can not be made
-	 */
-	public static Class isAssignableTo(final Class<?> fromValue, final Class<?> toValue, final String message) {
-		if (!toValue.isAssignableFrom(fromValue)) {
-			throw new ClassCastException(message);
-		}
-		return fromValue;
-	}
+    /**
+     * Check that a reference is not null, throwing a IllegalArgumentException if it is.
+     *
+     * @param reference    The reference to check
+     * @param errorMessage the message for the exception if the reference is null
+     * @param <T>          the type of the reference
+     * @throws java.lang.IllegalArgumentException if the reference is null
+     */
+    public static <T> T checkNotNull(final T reference, final String errorMessage) {
+        return precondition(reference, new Predicate<T>() {
+            @Override
+            public boolean test(T testValue) {
+                return testValue != null;
+            }
+        }, IllegalArgumentException.class, errorMessage);
+    }
+
+    /**
+     * Check that a String is not empty after removing whitespace.
+     *
+     * @param reference    the string
+     * @param errorMessage the message for the exception
+     * @throws java.lang.NullPointerException     if the String is null
+     * @throws java.lang.IllegalArgumentException if the string is non-null but zero length
+     */
+    public static String checkNonEmptyString(final String reference, final String errorMessage) {
+        return precondition(reference, new Predicate<String>() {
+            @Override
+            public boolean test(String testValue) {
+                for (Character c : checkNotNull(testValue, errorMessage).toCharArray()) {
+                    if (!Character.isWhitespace(c)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }, IllegalArgumentException.class, errorMessage);
+    }
+
+    /**
+     * Check that one class is assignable to another.
+     *
+     * @param reference the class to test
+     * @param toValue   the class assigning to
+     * @param message   the message used in the exception if thrown
+     * @throws ClassCastException if the assignment can not be made
+     */
+    public static Class isAssignableTo(final Class<?> reference, final Class<?> toValue, final String message) {
+        return precondition(reference, new Predicate<Class>() {
+            @Override
+            public boolean test(Class testValue) {
+                return toValue.isAssignableFrom(testValue);
+            }
+        }, ClassCastException.class, message);
+    }
 }
